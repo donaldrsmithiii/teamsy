c67d0592676211ef60b4b7485138e6d5c31efb4e
Donald Smith III <donaldsmithiii@Donalds-MacBook-Pro.local>
2018-11-23 13:14:01 +0800

    Started additional feature (hero) section animation items

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured from './featured';
 
+
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
+      
     </div>
   );
 }
